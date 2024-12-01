ad44041c4a86022b6f7c0b8b54f2b67289d24a78
jharris99-git <jharris99@my.bcit.ca>
2024-11-30 22:11:06 -0800

    Pre-org

--- src/01_train.py
+++ src/01_train.py
@@ -1,14 +1,42 @@
 # define training code
+import csv
 import gzip
 import pickle
 
+import numpy as np
+import torch
+
+# ~~~~~~~~~~~~~~~~~~~~~~~ General  Functions ~~~~~~~~~~~~~~~~~~~~~~~ #
 
 def load_pca_model():
     with gzip.open('../models/PCA.pkl.gz', 'rb') as f:
         return pickle.load(f)
 
+
 def pca_inverse_transform(pca, pca_100):
     return pca.inverse_transform(pca_100)
+
+
+def csv_files_to_nparray(file_list):
+    rows = []
+    for file_name in file_list:
+        with open(file_name, 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                # Convert row to float values and append to rows list
+                rows.append([float(value) for value in row])
+
+    # Convert the list of rows to a 2D NumPy array
+    return np.array(rows)
+
+
+def convert_to_cuda_tensor(np_array):
+    return torch.tensor(np_array, device='cuda')
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~ Joe's  Functions ~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ MAIN ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 if __name__ == '__main__':
